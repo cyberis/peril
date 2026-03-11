1605090079fa16cf198f0c510fe4dbca1e201f5e
Cyberis <christopher.bortz@gmail.com>
2026-03-10 22:07:08 -0400

    Add RabbitMQ Comms to the Server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"log"
+	"os"
+	"os/signal"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	fmt.Println("Starting Peril server ... ")
+	const rabbitConnString = "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(rabbitConnString)
+	if err != nil {
+		log.Fatalf("Could not connect to RabbitMQ: %v", err)
+
+	}
+	defer conn.Close()
+
+	fmt.Println("Peril game server connected to RabbitMQ!")
+
+	// wait for ctrl+c to quit
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("RabbitMQ connection closed.")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/cyberis/peril
 
 go 1.25.5
+
+require github.com/rabbitmq/amqp091-go v1.10.0 // indirect
